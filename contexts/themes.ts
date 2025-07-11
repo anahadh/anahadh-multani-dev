fc2e362e3d1c52accf3b902678227a5553f07273
anahadh <anahadh@hotmail.com>
2025-07-11 10:46:58 -0700

    adjusted themes for button hover

--- contexts/themes.ts
+++ contexts/themes.ts
@@ -1,23 +1,23 @@
 export const themes = {
     tidepool: {
         background: 'from-blue-500 to-green-500',
-        hoverBackground: 'hover:from-blue-600 hover:to-green-600',
+        hoverBackground: 'hover:ring-[7px] hover:ring-teal-500/50',
     },
     dreamscape: {
         background: 'from-purple-500 to-pink-500',
-        hoverBackground: 'hover:from-purple-600 hover:to-pink-600',
+        hoverBackground: 'hover:ring-[7px] hover:ring-pink-300/50',
     },
     firelight: {
         background: 'from-red-500 to-yellow-500',
-        hoverBackground: 'hover:from-red-600 hover:to-yellow-600',
+        hoverBackground: 'hover:ring-[7px] hover:ring-yellow-500/50',
     },
     skyburst: {
         background: 'from-pink-500 to-blue-500',
-        hoverBackground: 'hover:from-pink-600 hover:to-blue-600',
+        hoverBackground: 'hover:ring-[7px] hover:ring-blue-300/50',
     },
     nightfall: {
         background: 'from-orange-800 to-blue-900',
-        hoverBackground: 'hover:from-orange-700 hover:to-blue-800',
+        hoverBackground: 'hover:ring-[7px] hover:ring-gray-300/50',
     }
 }
 
