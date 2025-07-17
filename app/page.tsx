8ae085f689ffbdf4e99c5fd659e7a89baa9858f9
anahadh <anahadh@hotmail.com>
2025-07-17 12:24:19 -0700

    added extra content to about me section

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,12 @@ export default function Home() {
       <div className="w-screen bg-white" id="#about">
         <div className="flex flex-col items-center justify-center pt-20 pb-10">
           <AboutMeTitle />
-          <p className="text-3xl leading-relaxed text-justify font-notoSans font-semibold max-w-2xl">
-            My name is Anahadh Multani, and I'm a senior in high school with a heavy passion for web development. 
+          <p className="text-3xl leading-relaxed text-justify font-notoSans font-medium max-w-3xl">
+            My name is Anahadh Multani, and I'm a senior in high school from Redmond, Washington. During the pandemic, I started to build very simple websites using HTML and CSS. In high school, I started learning how to build websites more seriously, and since then, I've put an emphasis on learning new skills with every website that I build and building for NGOs and small businesses.
+            <br /><br />
+            The biggest project that I've worked on is the website for Incepta, a company which provided high schoolers the opportunity to find mentors doing their dream majors at their dream colleges and connect with them. I built the website's landing page and a few other UIs for the website, and it the experience provided me a launchpad to start developing websites for others.
+            <br /><br />
+            
           </p>
         </div>
       </div>
